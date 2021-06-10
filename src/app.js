2830e477ac3cde7d45c1f46466165516c6c17cdc
Samuel Garcia <samfrontdev@gmail.com>
2021-06-10 17:59:04 -0500

    Version 1.5 Lazy loading and Eliminate Button Integrated

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
+// Creacion de imagenes
+
 const imagesSection = document.querySelector("#images");
-const imageContainer = document.createElement("div");
+// const imageContainer = document.createElement("div");
 const button = document.querySelector("#daButton");
+const eliminateButton = document.querySelector("#daButton2");
+// imageContainer.className = "image-container";
 
-imageContainer.className = "image-container";
-
-imagesSection.appendChild(imageContainer);
+// imagesSection.appendChild(imageContainer);
 
 const maximum = 120;
 const minimum = 1;
@@ -16,15 +18,21 @@ const createImageNode = () => {
   image.style.width = "320px";
   image.className = "image";
   image.src = `https://source.unsplash.com/collection/${random()}`;
+  image.loading = "lazy";
 
   return image;
 };
-
 const newImage = createImageNode();
 const mountNode = document.getElementById("images");
-imageContainer.appendChild(newImage);
+// imageContainer.appendChild(newImage);
 
+//Agregar Imagenes
 button.onclick = () => {
   const nuevaImagen = createImageNode();
   mountNode.append(nuevaImagen);
+};
+
+// Eliminar Imagenes
+eliminateButton.onclick = () => {
+  mountNode.innerHTML = "";
 };
